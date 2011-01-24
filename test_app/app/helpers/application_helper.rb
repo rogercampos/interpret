b5e996c63a72e86d95a48209414df79de1975e30
Roger Campos <roger@itnig.net>
2011-01-24 23:53:07 +0100

    Adding highlighting of the current section in the sidebar

--- test_app/app/helpers/application_helper.rb
+++ test_app/app/helpers/application_helper.rb
@@ -1,10 +1,19 @@
 module ApplicationHelper
-  def show_tree(hash, prev_key = "")
+  def show_tree(hash, params_key, prev_key = "")
     out = "<ul>"
+    if params_key
+      params_key = params_key.split(".")
+      first_key = params_key.shift
+      params_key = params_key.join(".")
+    end
     hash.keys.each do |key|
-      out += "<li>#{link_to key, node_translations_path(:key => prev_key+key)}"
+      out += "<li>"
+      out += "<b>" if params_key && key == first_key
+      out += "#{link_to key, node_translations_path(:key => prev_key+key)}"
+      out += "</b>" if params_key && key == first_key
+
       if hash[key].present? && hash[key].is_a?(Hash)
-        out += show_tree(hash[key], "#{prev_key}#{key}.")
+        out += show_tree(hash[key], params_key, "#{prev_key}#{key}.")
       end
       out += "</li>"
     end
