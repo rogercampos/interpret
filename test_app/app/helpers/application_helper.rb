3fe051cc624f172f08f1f7122e49786ef50962b3
Roger Campos <roger@itnig.net>
2011-01-24 20:59:25 +0100

    Tree sidebar now contains links

--- test_app/app/helpers/application_helper.rb
+++ test_app/app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def show_tree(hash)
+  def show_tree(hash, prev_key = "")
     out = "<ul>"
     hash.keys.each do |key|
-      out += "<li>#{key}"
+      out += "<li>#{link_to key, node_translations_path(:key => prev_key+key)}"
       if hash[key].present? && hash[key].is_a?(Hash)
-        out += show_tree(hash[key])
+        out += show_tree(hash[key], "#{prev_key}#{key}.")
       end
       out += "</li>"
     end
