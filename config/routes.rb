16f15df9f43f8dbe9401b4977d451a92b5740e88
Roger Campos <roger@itnig.net>
2011-01-28 19:53:17 +0100

    Adding /interpret route, some style corrections for sidebar tree

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :interpret do
-    resources :translations, :except => [:new, :create, :destroy, :show]  do
+    resources :translations, :except => [:index, :new, :create, :destroy, :show]  do
       collection do
         get :node
         get :tree
@@ -15,28 +15,9 @@ Rails.application.routes.draw do
       end
     end
 
-    #match "tools", :to => "interpret/tools#index", :as => "interpret_tools"
     match "search", :to => "search#index"
     match "search_for", :to => "search#perform"
+
+    root :to => "translations#index"
   end
-  #scope Interpret.scope do
-    #resources :translations, :except => [:new, :create, :destroy, :show], :controller => "interpret/translations", :as => Interpret.resource_name do
-      #collection do
-        #get :node
-        #get :tree
-      #end
-    #end
-
-    #resources :tools, :only => :index, :controller => "interpret/tools", :as => "#{Interpret.scope}_tools" do
-      #collection do
-        #get :fetch
-        #post :upload
-        #post :migrate
-      #end
-    #end
-
-    ##match "tools", :to => "interpret/tools#index", :as => "interpret_tools"
-    #match "search", :to => "interpret/search#index", :as => "interpret_search"
-    #match "search_for", :to => "interpret/search#perform", :as => "interpret_search_for"
-  #end
 end
