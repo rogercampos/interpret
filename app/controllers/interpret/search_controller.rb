ae7080aa200301b82bca0d4df231cd4ddc6d450d
Roger Campos <roger@itnig.net>
2011-04-01 10:22:34 +0200

    making the search work with multiple words

--- app/controllers/interpret/search_controller.rb
+++ app/controllers/interpret/search_controller.rb
@@ -2,6 +2,8 @@ class Interpret::SearchController < Interpret::BaseController
 
   def perform
     t = Interpret::Translation.arel_table
-    @translations = Interpret::Translation.locale(I18n.locale).where(t[:key].matches("%#{CGI.escape(params[:key])}%").and(t[:value].matches("%#{CGI.escape(params[:value])}%"))  )
+    search_key = params[:key].split(" ").map{|x| "%#{CGI.escape(x)}%"}
+    search_value = params[:value].split(" ").map{|x| "%#{CGI.escape(x)}%"}
+    @translations = Interpret::Translation.locale(I18n.locale).where(t[:key].matches_all(search_key).or(t[:value].matches_all(search_value))  )
   end
 end
