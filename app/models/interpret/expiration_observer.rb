ff85412a847dbfb71ea3cfb4309d49036db0c92f
Roger Campos <roger@itnig.net>
2011-04-02 19:31:00 +0200

    passing the translation key to the custom expiration logic

--- app/models/interpret/expiration_observer.rb
+++ app/models/interpret/expiration_observer.rb
@@ -4,26 +4,26 @@ module Interpret
     observe Interpret::Translation
 
     def after_update(record)
-      run_expiration if record.value_changed?
+      run_expiration(record) if record.value_changed?
     end
 
     def after_create(record)
-      run_expiration
+      run_expiration(record)
     end
 
     def after_destroy(record)
-      run_expiration
+      run_expiration(record)
     end
 
   protected
     # expiration logic for your app
-    def expire_cache
+    def expire_cache(key)
     end
 
   private
-    def run_expiration
+    def run_expiration(record)
       Interpret.backend.reload! if Interpret.backend
-      expire_cache
+      expire_cache(record.key)
     end
   end
 end
