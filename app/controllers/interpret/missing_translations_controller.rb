1a24593476ea26ebde5798066d9585523c52f438
Roger Campos <roger@itnig.net>
2011-10-07 15:50:40 +0200

    Retrieve the translation records with AR, even if we have to make another query

--- app/controllers/interpret/missing_translations_controller.rb
+++ app/controllers/interpret/missing_translations_controller.rb
@@ -4,16 +4,18 @@ class Interpret::MissingTranslationsController < Interpret::BaseController
 
       case ActiveRecord::Base.connection.adapter_name
       when "Mysql2"
-        res = ActiveRecord::Base.connection.execute("select t.value, t.key from translations t where t.locale ='#{I18n.default_locale}' and (select count(*) from translations t2 where t2.key = t.key and t2.locale ='#{I18n.locale}') = 0")
-        @missing_translations = res.map{|x| {:ref_value => YAML.load(x[0]), :key => x[1]}}
+        res = ActiveRecord::Base.connection.execute("select t.id from translations t where t.locale ='#{I18n.default_locale}' and (select count(*) from translations t2 where t2.key = t.key and t2.locale ='#{I18n.locale}') = 0")
 
       when "SQLite"
-        res = ActiveRecord::Base.connection.execute("select t.value, t.key from translations t where t.locale ='#{I18n.default_locale}' and (select count(*) from translations t2 where t2.key = t.key and t2.locale ='#{I18n.locale}') = 0")
-        @missing_translations = res.map{|x| {:ref_value => YAML.load(x["value"]), :key => x["key"]}}
+        res = ActiveRecord::Base.connection.execute("select t.id from translations t where t.locale ='#{I18n.default_locale}' and (select count(*) from translations t2 where t2.key = t.key and t2.locale ='#{I18n.locale}') = 0")
 
       else
         raise NotImplementedError, "database adapter not supported"
       end
+
+      ids = res.map{|x| x.first}
+      translations = Interpret::Translation.where(:id => ids)
+      @missing_translations = translations.map{|x| {:ref_value => x.value, :key => x.key}}
     end
   end
 end
