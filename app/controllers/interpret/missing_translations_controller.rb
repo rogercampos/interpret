1cc9f0be801e4735b0e567611aefa1aed33083df
Roger Campos <roger@itnig.net>
2011-10-07 14:30:41 +0200

    Compatibility with sqlite and mysql2 adapters

--- app/controllers/interpret/missing_translations_controller.rb
+++ app/controllers/interpret/missing_translations_controller.rb
@@ -1,15 +1,19 @@
 class Interpret::MissingTranslationsController < Interpret::BaseController
   def index
     unless I18n.locale == I18n.default_locale
-      res = ActiveRecord::Base.connection.execute("select t.value, t.key from
-                                                  translations t where
-                                                  t.locale ='#{I18n.default_locale}'
-                                                  and (select count(*) from
-                                                       translations t2 where
-                                                       t2.key = t.key and
-                                                       t2.locale ='#{I18n.locale}') =
-                                                         0")
-      @missing_translations = res.map{|x| {:ref_value => YAML.load(x["value"]), :key => x["key"]}}
+
+      case ActiveRecord::Base.connection.adapter_name
+      when "Mysql2"
+        res = ActiveRecord::Base.connection.execute("select t.value, t.key from translations t where t.locale ='#{I18n.default_locale}' and (select count(*) from translations t2 where t2.key = t.key and t2.locale ='#{I18n.locale}') = 0")
+        @missing_translations = res.map{|x| {:ref_value => YAML.load(x[0]), :key => x[1]}}
+
+      when "SQLite"
+        res = ActiveRecord::Base.connection.execute("select t.value, t.key from translations t where t.locale ='#{I18n.default_locale}' and (select count(*) from translations t2 where t2.key = t.key and t2.locale ='#{I18n.locale}') = 0")
+        @missing_translations = res.map{|x| {:ref_value => YAML.load(x["value"]), :key => x["key"]}}
+
+      else
+        raise NotImplementedError, "database adapter not supported"
+      end
     end
   end
 end
