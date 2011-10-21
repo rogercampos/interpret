f3fd46e277076e8b12308e904d32e8218769e5bf
Roger Campos <roger@itnig.net>
2011-10-21 17:32:51 +0200

    Remove unused database helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,21 +2,12 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path('../../test_app/config/environment', __FILE__)
-require "rails/test_help"
 require "rspec/rails"
-require "database_helpers"
 
 ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = "test.com"
 
-Rails.backtrace_cleaner.remove_silencers!
-
-include DatabaseHelpers
-# Run any available migration
-puts 'Setting up database...'
-drop_all_tables
-migrate_database
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each{|f| require f}
@@ -27,7 +18,6 @@ RSpec.configure do |config|
   require 'rspec/expectations'
 
   config.include RSpec::Matchers
-  config.include DatabaseHelpers
 
   # == Mock Framework
   config.mock_with :rspec
