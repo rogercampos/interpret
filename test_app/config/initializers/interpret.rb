c8cc94f490a0c3cb296a658dc59eea69454d3c10
Roger Campos <roger@itnig.net>
2012-06-08 12:10:24 +0200

    Fix test app layout no longer exist

--- test_app/config/initializers/interpret.rb
+++ test_app/config/initializers/interpret.rb
@@ -1,7 +1,6 @@
 Interpret.configure do |config|
   config.registered_envs << :development
   config.parent_controller = "application_controller"
-  config.layout = "backoffice"
   config.ability = "interpret_ability"
   config.black_list = ["blacklist.*", "missings.black"]
 end
