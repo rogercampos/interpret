3971f9d6677c0a4c917f364e8302901d82dc44d1
Albert Bellonch Llargués <albert@itnig.net>
2013-02-13 14:08:09 +0100

    Remove validation

--- app/models/interpret/translation.rb
+++ app/models/interpret/translation.rb
@@ -2,7 +2,6 @@ module Interpret
 
   class Translation < I18n::Backend::ActiveRecord::Translation
     default_scope order('locale ASC')
-    validates_presence_of :value
 
     class << self
       # Generates a hash representing the tree structure of the translations
